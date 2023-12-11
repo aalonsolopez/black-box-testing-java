2e5b2ef2e62ab171ed533d1a2e42bd6c3c267949
Alejandro Alonso López <aalonsopersonal@gmail.com>
2023-12-11 22:12:25 +0100

    Update Maven compiler source and target versions, add JUnit dependency, and create BST test suite

--- src/test/java/org/aalonso/InsertSuite/InsertTest.java
+++ src/test/java/org/aalonso/InsertSuite/InsertTest.java
@@ -2,32 +2,43 @@ package org.aalonso.InsertSuite;
 
 import com.bst.BST;
 import com.bst.Node;
-
 import com.exceptions.DepthException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 
 public class InsertTest {
-    BST<Integer> tree;
     @ParameterizedTest
-    @CsvFileSource(resources = "./values.csv", numLinesToSkip = 1, lineSeparator = "\n", delimiter = ',')
-
-    @BeforeEach
-    public void setUp() {
-        tree = new BST();
-    }
-
-    public void insertValuesTillDepth(BST<Integer> tree, int depth, boolean recursive) throws DepthException {
-        for (int i = 0; i < depth - 1; i++) {
-            tree.insert(i, recursive);
+    @CsvFileSource(resources = "/values.csv", numLinesToSkip = 1, lineSeparator = "\n", delimiter = ',')
+    public void testInsertLimitValues(int content, boolean recursive, int depth) throws DepthException {
+        BST<Integer> tree = new BST<Integer>();
+        if (content >= -2500 && content <= 2500) {
+            tree.insert(content, false);
+            Node<Integer> node = tree.search(content);
+            assertNotNull(node);
+        } else {
+            assertThrows(IllegalArgumentException.class, () -> tree.insert(content, false));
         }
     }
 
-    @Test
-    public void testInsert() throws DepthException {
-        // TODO: Add test
+    @ParameterizedTest
+    @CsvFileSource(resources = "/values.csv", numLinesToSkip = 1, lineSeparator = "\n", delimiter = ',')
+    public void testInsertLimitDepth(int content, boolean recursive, int depth) throws DepthException {
+        BST<Integer> tree = new BST<Integer>();
+        int startingValue = 0;
+        for (int i = 0; i < depth - 1; i++) {
+            tree.insert(startingValue++, recursive);
+        }
+        if (tree.depth() >= 50) {
+            assertThrows(DepthException.class, () -> tree.insert(2500, recursive));
+        } else {
+            tree.insert(2500, recursive);
+            Node<Integer> node = tree.search(2500);
+            assertNotNull(node);
+        }
     }
 }
---
+++ src/test/java/org/aalonso/TestSuiteBST.java
@@ -0,0 +1,12 @@
+package org.aalonso;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+@Suite
+@SuiteDisplayName("BST Test Suite")
+@SelectPackages("org.aalonso.InsertSuite")
+public class TestSuiteBST {
+
+}
