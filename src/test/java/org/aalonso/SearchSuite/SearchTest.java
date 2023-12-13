fb6490337898046c344e48f59a5a8fa9e88ea655
Alejandro Alonso López <aalonsopersonal@gmail.com>
2023-12-13 17:33:49 +0100

    Add parameterized test for search functionality

--- src/test/java/org/aalonso/SearchSuite/SearchTest.java
+++ src/test/java/org/aalonso/SearchSuite/SearchTest.java
@@ -1,5 +1,25 @@
 package org.aalonso.SearchSuite;
 
+import com.bst.BST;
+import com.bst.Node;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+
 public class SearchTest {
-    
+    @ParameterizedTest
+    @CsvFileSource(resources = "/searchSuiteResources/values.csv", numLinesToSkip = 1, lineSeparator = "\n", delimiter = ',')
+    public void testSearchLimitValues(int content) {
+        BST<Integer> tree = (content >= -2500 && content <= 2500) ? new BST<Integer>(content) : new BST<Integer>();
+        if (content >= -2500 && content <= 2500) {
+            Node<Integer> node = tree.search(content);
+            assertNotNull(node);
+        } else {
+            assertNull(tree.search(content));
+        }
+    }
 }
